d0b7ddd35f5d39897bf9c0b8a4b9d521173e51b6
bkrusz <bkrusz@icloud.com>
2020-10-07 18:20:20 -0700

    declare functions in AddingMachine.java

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Constructor that sets initial total and calculation history to 0
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * Returns the current total
+     * @return total the current total
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * Adds the input to both the total and the calculation history
+     * @param value value to be added
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * Subtracts the input to both the total and the calculation history
+     * @param value value to be subtracted
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * Returns the calculation history
+     * @return history the calculation history
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * Sets the total and calculation history to 0
+     */
+    public void clear() {
+    }
+}
